df0f36ccc761c56722acaa0fdd3f74b647ce8bef
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-11 13:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+
+
+console.log(findFirstNonRepeatingCharacter('hello world')); 
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+}
+let str = prompt("Enter a string");
+alert(findFirstNonRepeatingCharacter(str));
